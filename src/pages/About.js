11612cc26f172523ca8d33a19841e63563cc11ce
Vaibhav Yadav <tron21vaibhav@gmail.com>
2023-06-30 23:30:12 +0530

    Minor Changes

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+
+
+
+export default function ABbout() {
+    let me
+
+
+
+    return (
+        <div>
+            <div className="AboutContainer">
+                <div className="AboutTextContainer">
+                    <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                    <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.(Hitch costs extra 😉)Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+                    <h1>What Are You Waiting For!</h1>
+                    <p>Learning By Vaibhav Yadav</p>
+                </div>
+
+                <div className="AboutTextContainer">
+                    <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+                    <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.(Hitch costs extra 😉)Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+                    <h1>What Are You Waiting For!</h1>
+                    <p>Learning By Vaibhav Yadav</p>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+    }
